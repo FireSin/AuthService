01f668b87245a5dc69464c79ea20b1036168fbb3
Кубанцев Артур Вадимович <avakubantsev@sberbank.ru>
2023-07-16 11:03:13 +0300

    init

--- src/main/java/ru/firesin/authService/repository/UserRepository.java
+++ src/main/java/ru/firesin/authService/repository/UserRepository.java
@@ -2,11 +2,9 @@ package ru.firesin.authService.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import ru.firesin.authService.bd.User;
-
-import java.util.Optional;
+import ru.firesin.authService.bd.AuthUser;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Long> {
-    User findByToken(String token);
+public interface UserRepository extends JpaRepository<AuthUser, Long> {
+    AuthUser findByToken(String token);
 }
--- src/main/java/ru/firesin/authService/services/Autoriz.java
+++ src/main/java/ru/firesin/authService/services/Autoriz.java
@@ -1,32 +1,58 @@
 package ru.firesin.authService.services;
 
-import lombok.SneakyThrows;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import ru.firesin.authService.annotations.MySecured;
+import ru.firesin.authService.bd.AuthUser;
+import ru.firesin.authService.dto_entry.UserJson;
+import ru.firesin.authService.repository.UserRepository;
 
-import java.util.Map;
+import java.security.SecureRandom;
+import java.util.Base64;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
 @RestController
 @RequestMapping
 public class Autoriz {
+    @Autowired
+    private UserRepository userRepository;
 
-    Map<String, String> authMap;
-
-    @SneakyThrows
-    @GetMapping("/login")
-    @MySecured("localUser")
-    public void login(HttpServletRequest request, HttpServletResponse response) {
-        System.out.println(request.getQueryString());
-        response.addHeader("Token", "123456");
+    @PostMapping("/login")
+    public ResponseEntity<String> login(@RequestBody UserJson userJson) {
+        AuthUser authUser = new AuthUser();
+        authUser.setName(userJson.getName());
+        authUser.setPassword(userJson.getPassword());
+        authUser.setRole("localUser");
+        authUser.setToken(generateToken());
+        try {
+            userRepository.save(authUser);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
+        }
+        HttpHeaders headers = new HttpHeaders();
+        headers.add(HttpHeaders.AUTHORIZATION, "Bearer " + authUser.getToken()); // Добавляем токен в заголовок
+        return ResponseEntity.ok().headers(headers).body("Success");
     }
 
-    @MySecured("localUser")
+    private String generateToken() {
+        byte[] randomBytes = new byte[32];
+        SecureRandom secureRandom = new SecureRandom();
+        secureRandom.nextBytes(randomBytes);
+        return Base64.getUrlEncoder().withoutPadding().encodeToString(randomBytes);
+    }
+
     @GetMapping("/entry")
+    @MySecured("localUser")
     public String entry(HttpServletRequest request) {
-        return "Follow the white rabbit....";
+        String token = request.getHeader("Token");
+        var user = userRepository.findByToken(token);
+        try {
+            return "Follow the white rabbit. \nKnock, knock, " + user.getName();
+        } catch (Exception e) {
+            return e.getMessage();
+        }
     }
 }
